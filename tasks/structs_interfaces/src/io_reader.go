114f700215f69028d79bd42d61b1aaae8486dfcd
Serge S. Yuriev <me@nevian.org>
2023-05-16 23:18:27 +0300

    homework ep2

--- tasks/structs_interfaces/src/io_reader.go
+++ tasks/structs_interfaces/src/io_reader.go
@@ -1,7 +1,9 @@
 package src
 
 import (
+	"bytes"
 	"io"
+	"strings"
 )
 
 type Reader interface {
@@ -16,7 +18,45 @@ type CountingToLowerReaderImpl struct {
 }
 
 func (cr *CountingToLowerReaderImpl) Read(p []byte) (int, error) {
-	return 0, nil
+	var (
+		n   int
+		err error
+	)
+
+	if n, err = cr.Reader.Read(p); err == nil || err == io.EOF {
+		cr.TotalBytesRead += int64(n)
+		copy(p, bytes.ToLower(p))
+		return n, err
+	}
+	return 0, err
+}
+
+func (cr *CountingToLowerReaderImpl) BytesRead() int64 {
+	return cr.TotalBytesRead
+}
+
+func (cr *CountingToLowerReaderImpl) ReadAll(bufSize int) (string, error) {
+	var (
+		n   int
+		err error
+		res strings.Builder
+		buf = make([]byte, bufSize)
+	)
+
+	res.Grow(bufSize)
+	for {
+		n, err = cr.Read(buf)
+		if err != nil {
+			break
+		}
+		res.Write(buf[:n])
+	}
+
+	if err == io.EOF {
+		return res.String(), nil
+	}
+
+	return res.String(), err
 }
 
 func NewCountingReader(r io.Reader) *CountingToLowerReaderImpl {
